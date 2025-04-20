eaae9c70a35df37c19985fe1d14967f4e31d6264
Dmitry <khv1702@gmail.com>
2025-04-21 02:02:15 +0300

    Added interface

---
+++ src/components/ui/article-item/article-item.interface.ts
@@ -0,0 +1,17 @@
+export interface IArticleItem {
+	author: {
+		username: string;
+		image: string;
+		following: boolean;
+	};
+	body?: string;
+	createdAt: string;
+	description: string;
+	favorited: boolean;
+	favoritesCount: number;
+	slug: string;
+	tagList: string[];
+	title: string;
+	updatedAt: string;
+	type: string;
+}
