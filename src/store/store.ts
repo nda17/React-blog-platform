bec4499c48bb097075187bf5c0e01927b5177a90
Dmitry <khv1702@gmail.com>
2025-04-22 11:30:24 +0300

    Added store-config

---
+++ src/store/store.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line
+//@ts-nocheck
+import { api } from '@/store/api';
+import { rootReducer } from '@/store/rootReducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { RootState } from '@reduxjs/toolkit/query';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { ThunkMiddleware } from 'redux-thunk';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: false
+		}).concat(api.middleware as ThunkMiddleware<RootState>)
+});
+
+const persistor = persistStore(store);
+
+export { persistor, store };
