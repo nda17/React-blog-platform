581af0c0872105b4202f2f8d33c6165bba049710
Dmitry <khv1702@gmail.com>
2025-05-01 02:46:26 +0300

    Added preloader

--- src/components/pages/articles-list/ArticlesList.tsx
+++ src/components/pages/articles-list/ArticlesList.tsx
@@ -1,18 +1,19 @@
 import styles from '@/components/pages/articles-list/ArticlesList.module.scss';
 import { IArticleItem } from '@/components/ui/article-item/article-item.interface';
 import { ArticleItem } from '@/components/ui/article-item/ArticleItem';
+import { Preloader } from '@/components/ui/preloader/Preloader';
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
- const ArticlesList: FC = () => {
+const ArticlesList: FC = () => {
 	const type = 'list';
 	const listArticles = useSelector(
 		(state: { listArticles: { articles: IArticleItem[] } }) =>
 			state.listArticles.articles
 	);
 
-	return (
+	return listArticles?.length ? (
 		<div className={styles.wrapper}>
 			{listArticles.map(item => {
 				return (
@@ -31,6 +32,8 @@ import { v4 as uuidv4 } from 'uuid';
 				);
 			})}
 		</div>
+	) : (
+		<Preloader />
 	);
 };
 
